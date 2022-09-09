a3c3e6fa83f91400cd8d396d87102f18436e4a17
Pintta <69728770+Pintta@users.noreply.github.com>
2022-09-09 23:44:00 +0300

    Update client.lua

--- resources/mumble-voip/client.lua
+++ resources/mumble-voip/client.lua
@@ -1,23 +1,18 @@
 local voiceData = {}
 local radioData = {}
 local callData = {}
-
 local playerServerId = GetPlayerServerId(PlayerId())
 
--- Functions
 function SetVoiceData(key, value)
 	TriggerServerEvent("mumble:SetVoiceData", key, value)
 end
 
--- Events
 RegisterNetEvent("mumble:SetVoiceData")
 AddEventHandler("mumble:SetVoiceData", function(voice, radio, call)
 	voiceData = voice
-
 	if radio then
 		radioData = radio
 	end
-
 	if call then
 		callData = call
 	end
@@ -38,11 +33,9 @@ AddEventHandler("onClientResourceStart", function (resourceName)
 	if GetCurrentResourceName() ~= resourceName then
 		return
 	end
-
 	TriggerServerEvent("mumble:Initialise")
 end)
 
--- Simulate PTT when radio is active
 Citizen.CreateThread(function()
 	while true do
 		Citizen.Wait(0)
@@ -52,7 +45,6 @@ Citizen.CreateThread(function()
 		local playerRadioActive = false
 		local playerCall = 0
 		local playerCallSpeaker = false
-
 		if playerData ~= nil then
 			playerMode = playerData.mode or 2
 			playerRadio = playerData.radio or 0
@@ -60,29 +52,23 @@ Citizen.CreateThread(function()
 			playerCall = playerData.call or 0
 			playerCallSpeaker = playerData.callSpeaker or false
 		end
-
-		if playerRadioActive then -- Force PTT enabled
+		if playerRadioActive then
 			SetControlNormal(0, 249, 1.0)
 			SetControlNormal(1, 249, 1.0)
 			SetControlNormal(2, 249, 1.0)
 		end
-
 		if IsControlJustPressed(0, mumbleConfig.controls.proximity.key) then
 			if mumbleConfig.controls.speaker.key == mumbleConfig.controls.proximity.key and not ((mumbleConfig.controls.speaker.secondary == nil) and true or IsControlPressed(0, mumbleConfig.controls.speaker.secondary)) then
 				local voiceMode = playerMode
-			
 				local newMode = voiceMode + 1
-			
 				if newMode > #mumbleConfig.voiceModes then
 					voiceMode = 1
 				else
 					voiceMode = newMode
 				end
-			
 				SetVoiceData("mode", voiceMode)
 			end
 		end
-
 		if mumbleConfig.radioEnabled then
 			if not mumbleConfig.controls.radio.pressed then
 				if IsControlJustPressed(0, mumbleConfig.controls.radio.key) then
@@ -90,12 +76,10 @@ Citizen.CreateThread(function()
 						SetVoiceData("radioActive", true)
 						playerData.radioActive = true
 						mumbleConfig.controls.radio.pressed = true
-
 						Citizen.CreateThread(function()
 							while IsControlPressed(0, mumbleConfig.controls.radio.key) do
 								Citizen.Wait(0)
 							end
-
 							SetVoiceData("radioActive", false)
 							playerData.radioActive = false
 							mumbleConfig.controls.radio.pressed = false
@@ -109,7 +93,6 @@ Citizen.CreateThread(function()
 				playerData.radioActive = false
 			end
 		end
-
 		if mumbleConfig.radioSpeakerEnabled then
 			if ((mumbleConfig.controls.speaker.secondary == nil) and true or IsControlPressed(0, mumbleConfig.controls.speaker.secondary)) then
 				if IsControlJustPressed(0, mumbleConfig.controls.speaker.key) then
@@ -122,7 +105,6 @@ Citizen.CreateThread(function()
 	end
 end)
 
--- UI
 Citizen.CreateThread(function()
 	while true do
 		Citizen.Wait(200)
@@ -134,7 +116,6 @@ Citizen.CreateThread(function()
 		local playerRadioActive = false
 		local playerCall = 0
 		local playerCallSpeaker = false
-
 		if playerData ~= nil then
 			playerMode = playerData.mode or 2
 			playerRadio = playerData.radio or 0
@@ -142,39 +123,23 @@ Citizen.CreateThread(function()
 			playerCall = playerData.call or 0
 			playerCallSpeaker = playerData.callSpeaker or false
 		end
-
-		-- Update UI
-		SendNUIMessage({
-			talking = playerTalking,
-			mode = mumbleConfig.voiceModes[playerMode][2],
-			radio = mumbleConfig.radioChannelNames[playerRadio] ~= nil and mumbleConfig.radioChannelNames[playerRadio] or playerRadio,
-			radioActive = playerRadioActive,
-			call = playerCall,
-			speaker = playerCallSpeaker,
-		})
+		SendNUIMessage({talking = playerTalking, mode = mumbleConfig.voiceModes[playerMode][2], radio = mumbleConfig.radioChannelNames[playerRadio] ~= nil and mumbleConfig.radioChannelNames[playerRadio] or playerRadio, radioActive = playerRadioActive, call = playerCall, speaker = playerCallSpeaker})
 	end
 end)
 
--- Main thread
 Citizen.CreateThread(function()
 	local talkingAnim = { "mic_chatter", "mp_facial" }
 	local normalAnim = { "mood_normal_1", "facials@gen_male@base" }
-
 	RequestAnimDict(talkingAnim[3])
-
 	while not HasAnimDictLoaded(talkingAnim[2]) do
 		Citizen.Wait(150)
 	end
-
 	RequestAnimDict(normalAnim[2])
-
 	while not HasAnimDictLoaded(normalAnim[2]) do
 		Citizen.Wait(150)
 	end
-
 	while true do
 		Citizen.Wait(500)
-
 		local playerId = PlayerId()
 		local playerPed = PlayerPedId()
 		local playerPos = GetPedBoneCoords(playerPed, headBone)
@@ -182,33 +147,27 @@ Citizen.CreateThread(function()
 		local playerData = voiceData[playerServerId]
 		local playerRadio = 0
 		local playerCall = 0
-
 		if playerData ~= nil then
 			playerRadio = playerData.radio or 0
 			playerCall = playerData.call or 0
 		end
-
 		local voiceList = {}
 		local muteList = {}
 		local callList = {}
 		local radioList = {}
-
-		for i = 1, #playerList do -- Proximity based voice (probably won't work for infinity?)
+		for i = 1, #playerList do
 			local remotePlayerId = playerList[i]
-
 			if playerId ~= remotePlayerId then
 				local remotePlayerServerId = GetPlayerServerId(remotePlayerId)
 				local remotePlayerPed = GetPlayerPed(remotePlayerId)
 				local remotePlayerPos = GetPedBoneCoords(remotePlayerPed, headBone)
 				local remotePlayerData = voiceData[remotePlayerServerId]
-
 				local distance = #(playerPos - remotePlayerPos)
 				local mode = 2
 				local radio = 0
 				local radioActive = false
 				local call = 0
 				local callSpeaker = false
-
 				if remotePlayerData ~= nil then
 					mode = remotePlayerData.mode or 2
 					radio = remotePlayerData.radio or 0
@@ -216,23 +175,14 @@ Citizen.CreateThread(function()
 					call = remotePlayerData.call or 0
 					callSpeaker = remotePlayerData.callSpeaker or false
 				end
-
-				-- Check if player is in range
 				if distance < mumbleConfig.voiceModes[mode][1] then
 					local volume = 1.0 - (distance / mumbleConfig.voiceModes[mode][1])^0.5
-
 					if volume < 0 then
 						volume = 0.0
 					end
-
-					voiceList[#voiceList + 1] = {
-						id = remotePlayerServerId,
-						player = remotePlayerId,
-						volume = volume,
-					}
-
+					voiceList[#voiceList + 1] = {id = remotePlayerServerId, player = remotePlayerId, volume = volume}
 					if mumbleConfig.callSpeakerEnabled then
-						if call > 0 then -- Collect all players in the phone call
+						if call > 0 then
 							if callSpeaker then
 								local callParticipants = callData[call]
 								if callParticipants ~= nil then
@@ -245,9 +195,8 @@ Citizen.CreateThread(function()
 							end
 						end
 					end
-					
 					if mumbleConfig.radioSpeakerEnabled then
-						if radio > 0 then -- Collect all players in the radio channel
+						if radio > 0 then
 							local radioParticipants = radioData[radio]
 							if radioParticipants then
 								for id, _ in pairs(radioParticipants) do
@@ -259,47 +208,32 @@ Citizen.CreateThread(function()
 						end
 					end
 				else
-					muteList[#muteList + 1] = {
-						id = remotePlayerServerId,
-						player = remotePlayerId,
-						volume = 0.0,
-						radio = radio,
-						radioActive = radioActive,
-						distance = distance,
-						call = call,
-					}					
+					muteList[#muteList + 1] = {id = remotePlayerServerId, player = remotePlayerId, volume = 0.0, radio = radio, radioActive = radioActive, distance = distance, call = call}					
 				end
 			end
 		end
-
 		for j = 1, #voiceList do
 			MumbleSetVolumeOverride(voiceList[j].player, voiceList[j].volume)
 		end
-
 		for j = 1, #muteList do
 			if callList[muteList[j].id] or radioList[muteList[j].id] then
 				if muteList[j].distance < mumbleConfig.speakerRange then
 					muteList[j].volume = 1.0 - (muteList[j].distance / mumbleConfig.speakerRange)^0.5
 				end
 			end
-
 			if muteList[j].radio > 0 and muteList[j].radio == playerRadio and muteList[j].radioActive then
 				muteList[j].volume = 1.0
 			end
-
 			if muteList[j].call > 0 and muteList[j].call == playerCall then
 				muteList[j].volume = 1.2
 			end
-			
 			MumbleSetVolumeOverride(muteList[j].player, muteList[j].volume)
 		end
 	end
 end)
 
--- Exports
 function SetRadioChannel(channel)
 	local channel = tonumber(channel)
-
 	if channel ~= nil then
 		SetVoiceData("radio", channel)
 	end
@@ -307,7 +241,6 @@ end
 
 function SetCallChannel(channel)
 	local channel = tonumber(channel)
-
 	if channel ~= nil then
 		SetVoiceData("call", channel)
 	end
