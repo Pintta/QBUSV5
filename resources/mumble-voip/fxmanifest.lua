933517cb795e953d3ab6965718fd7dfe4086dde6
Pintta <69728770+Pintta@users.noreply.github.com>
2022-09-09 23:37:09 +0300

    Update fxmanifest.lua

--- resources/mumble-voip/fxmanifest.lua
+++ resources/mumble-voip/fxmanifest.lua
@@ -1,26 +1,14 @@
-fx_version "adamant"
-game "gta5"
+fx_version 'cerulean'
+game 'gta5'
 
-name "mumble-voip"
-description "A tokovoip replacement that uses fivems mumble voip"
-author "Frazzle (frazzle9999@gmail.com)"
-version "1.1"
+shared_script 'config.lua'
+client_script 'client.lua'
+server_script 'server.lua'
 
-ui_page "ui/index.html"
-
+ui_page 'ui/index.html'
 files {
-	"ui/index.html",
-	"ui/mic_click_on.ogg",
-	"ui/mic_click_off.ogg",
+	'ui/index.html",
+	'ui/mic_click_on.ogg',
+	'ui/mic_click_off.ogg',
 }
-
-client_scripts {
-	"config.lua",
-    "client.lua",
-}
-
-server_scripts {
-    "server.lua",
-}
-
-provide "tokovoip_script"
+provide 'tokovoip_script'
