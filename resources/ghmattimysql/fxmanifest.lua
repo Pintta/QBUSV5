4509d54e8e0926f145ddc51d6978a5fc21b86f1d
Pintta <69728770+Pintta@users.noreply.github.com>
2022-09-09 23:35:10 +0300

    Update fxmanifest.lua

--- resources/ghmattimysql/fxmanifest.lua
+++ resources/ghmattimysql/fxmanifest.lua
@@ -1,30 +1,19 @@
 fx_version 'cerulean'
 game 'common'
-
-server_scripts {
-  'ghmattimysql-server.js',
-  'ghmattimysql-server.lua',
-}
-
+ui_page 'ui/index.html'
 client_script 'ghmattimysql-client.js'
 
-files {
-  'ui/index.html',
-  'ui/app.js',
-  'ui/app.css',
-  'ui/app.css',
-  'ui/fonts/alegreya-sans-v9-latin-300.woff',
-  'ui/fonts/alegreya-sans-v9-latin-500.woff',
-  'ui/fonts/alegreya-sans-v9-latin-700.woff',
-  'ui/fonts/alegreya-sans-v9-latin-regular.woff',
-  'ui/fonts/alegreya-sans-v9-latin-300.woff2',
-  'ui/fonts/alegreya-sans-v9-latin-500.woff2',
-  'ui/fonts/alegreya-sans-v9-latin-700.woff2',
-  'ui/fonts/alegreya-sans-v9-latin-regular.woff2',
-  'ui/fonts/MaterialIcons-Regular.eot',
-  'ui/fonts/MaterialIcons-Regular.ttf',
-  'ui/fonts/MaterialIcons-Regular.woff',
-  'ui/fonts/MaterialIcons-Regular.woff2',
+server_scripts {
+    'ghmattimysql-server.js',
+    'ghmattimysql-server.lua',
 }
 
-ui_page 'ui/index.html'
+files {
+    'ui/index.html',
+    'ui/app.js',
+    'ui/app.css',
+    'ui/fonts/*.woff',
+    'ui/fonts/*.woff2',
+    'ui/fonts/*.eot',
+    'ui/fonts/*.ttf',
+}
