2f2183e341269c596b238422b01a5fd86434f2df
Pintta <69728770+Pintta@users.noreply.github.com>
2022-09-13 01:51:18 +0300

    Add files via upload

---
+++ resources/[else]/prison/config.lua
@@ -0,0 +1,113 @@
+Config = {}
+
+Config.Jobs = {
+    ["electrician"] = "Elektricien",
+}
+
+Config.Locations = {
+    jobs = {
+        ["electrician"] = {
+            [1] = {
+                coords = {x = 1761.46, y = 2540.41, z = 45.56, h = 272.249},
+            },
+            [2] = {
+                coords = {x = 1718.54, y = 2527.802, z = 45.56, h = 272.249},
+            },
+            [3] = {
+                coords = {x = 1700.199, y = 2474.811, z = 45.56, h = 272.249},
+            },
+            [4] = {
+                coords = {x = 1664.827, y = 2501.58, z = 45.56, h = 272.249},
+            },
+            [5] = {
+                coords = {x = 1621.622, y = 2509.302, z = 45.56, h = 272.249},
+            },
+            [6] = {
+                coords = {x = 1627.936, y = 2538.393, z = 45.56, h = 272.249},
+            },
+            [7] = {
+                coords = {x = 1625.1, y = 2575.988, z = 45.56, h = 272.249},
+            },
+        },
+    },
+    ["freedom"] = {
+        coords = {x = 1782.03, y = 2590.11, z = 45.71, h = 356.5},
+    },
+    ["outside"] = {
+        coords = {x = 1839.18, y = 2591.08, z = 45.89, h = 184.5},
+    },
+    ["yard"] = {
+        coords = {x = 1773.85, y = 2677.7, z = 45.61, h = 179.685},
+    },
+    ["middle"] = {
+        coords = {x = 1693.33, y = 2569.51, z = 45.55, h = 123.5},
+    },
+    ["shop"] = {
+        coords = {x = 1775.92, y = 2587.74, z = 45.71, h = 0.5, r = 1.0},
+    },
+    spawns = {
+        [1] = {
+            animation = "bumsleep",
+            coords = {x = 1661.046, y = 2524.681, z = 45.564, h = 260.545},
+        },
+        [2] = {
+            animation = "lean",
+            coords = {x = 1650.812, y = 2540.582, z = 45.564, h = 230.436},
+        },
+        [3] = {
+            animation = "lean",
+            coords = {x = 1654.959, y = 2545.535, z = 45.564, h = 230.436},
+        },
+        [4] = {
+            animation = "lean",
+            coords = {x = 1697.106, y = 2525.558, z = 45.564, h = 187.208},
+        },
+        [5] = {
+            animation = "chair4",
+            coords = {x = 1673.084, y = 2519.823, z = 45.564, h = 229.542},
+        },
+        [6] = {
+            animation = "chair",
+            coords = {x = 1666.029, y = 2511.367, z = 45.564, h = 233.888},
+        },
+        [7] = {
+            animation = "chair4",
+            coords = {x = 1691.229, y = 2509.635, z = 45.564, h = 52.432},
+        },
+        [8] = {
+            animation = "finger2",
+            coords = {x = 1770.59, y = 2536.064, z = 45.564, h = 258.113},
+        },
+        [9] = {
+            animation = "smoke",
+            coords = {x = 1787.64, y = 2699.92, z = 45.64, h = 146.5},
+        },
+        [10] = {
+            animation = "smoke",
+            coords = {x = 1765.29, y = 2577.96, z = 50.54, h = 270.5, r = 1.0}, 
+        },
+        [11] = {
+            animation = "smoke",
+            coords = {x = 1786.14, y = 2577.42, z = 50.54, h = 278.5, r = 1.0}, 
+        },
+    }
+}
+
+Config.CanteenItems = {
+    [1] = {
+        name = "sandwich",
+        price = 4,
+        amount = 500,
+        info = {},
+        type = "item",
+        slot = 1,
+    },
+    [2] = {
+        name = "water_bottle",
+        price = 4,
+        amount = 500,
+        info = {},
+        type = "item",
+        slot = 2,
+    },
+}
