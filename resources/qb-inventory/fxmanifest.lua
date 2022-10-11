281ec622e807979c0635b54f15aa6fcc2c57c8b1
Pintta <69728770+Pintta@users.noreply.github.com>
2022-10-11 14:42:37 +0300

    Create fxmanifest.lua

---
+++ resources/qb-inventory/fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'adamant'
+game 'gta5
+shared_script 'config.lua
+server_script 'server/main.lua'
+client_script 'client/main.lua'
+ui_page 'html/ui.html'
+files {
+	'html/ui.html',
+	'html/css/main.css',
+	'html/js/app.js',
+	'html/images/*.png',
+	'html/images/*.jpg',
+	'html/ammo_images/*.png',
+	'html/attachment_images/*.png',
+	'html/*.ttf',
+}
