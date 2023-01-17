8651efba85e6a1173a3ec631f395eb6190171774
Pintta <69728770+Pintta@users.noreply.github.com>
2023-01-17 17:21:03 +0200

    Update main.lua

--- resources/qb-justice/server/main.lua
+++ resources/qb-justice/server/main.lua
@@ -1,7 +1,10 @@
 QBCore = nil
-TriggerEvent('QBCore:GetObject', function(obj) QBCore = obj end)
 
-QBCore.Commands.Add("setlawyer", "Set someone as a lawyer", {{name="id", help="Player ID"}}, true, function(source, args)
+TriggerEvent('QBCore:GetObject', function(obj)
+	QBCore = obj
+end)
+
+QBCore.Commands.Add('setlawyer', 'Set someone as a lawyer', {{name = 'id', help = 'ID'}}, true, function(source, args)
     local Player = QBCore.Functions.GetPlayer(source)
     local playerId = tonumber(args[1])
     local OtherPlayer = QBCore.Functions.GetPlayer(playerId)
@@ -13,39 +16,39 @@ QBCore.Commands.Add("setlawyer", "Set someone as a lawyer", {{name="id", help="P
                 lastname = OtherPlayer.PlayerData.charinfo.lastname,
                 citizenid = OtherPlayer.PlayerData.citizenid,
             }
-            OtherPlayer.Functions.SetJob("lawyer")
-            OtherPlayer.Functions.AddItem("lawyerpass", 1, false, lawyerInfo)
-            TriggerClientEvent("QBCore:Notify", source, "You was" .. OtherPlayer.PlayerData.charinfo.firstname .. " " .. OtherPlayer.PlayerData.charinfo.lastname .. " hired as a lawyer")
-            TriggerClientEvent("QBCore:Notify", OtherPlayer.PlayerData.source, "Now you are a lawyer.")
-            TriggerClientEvent('inventory:client:ItemBox', OtherPlayer.PlayerData.source, QBCore.Shared.Items["lawyerpass"], "add")
+            OtherPlayer.Functions.SetJob('lawyer')
+            OtherPlayer.Functions.AddItem('lawyerpass', 1, false, lawyerInfo)
+            TriggerClientEvent('QBCore:Notify', source, 'You was ' .. OtherPlayer.PlayerData.charinfo.firstname .. ' ' .. OtherPlayer.PlayerData.charinfo.lastname .. ' hired as a lawyer')
+            TriggerClientEvent('QBCore:Notify', OtherPlayer.PlayerData.source, 'Now you are a lawyer.')
+            TriggerClientEvent('inventory:client:ItemBox', OtherPlayer.PlayerData.source, QBCore.Shared.Items['lawyerpass'], 'add')
         else
-            TriggerClientEvent("QBCore:Notify", source, "This person is not present.", "error")
+            TriggerClientEvent('QBCore:Notify', source, 'This person is not present.', 'error')
         end
     else
-        TriggerClientEvent("QBCore:Notify", source, "You have no rights ...", "error")
+        TriggerClientEvent('QBCore:Notify', source, 'You have no rights..', 'error')
     end
 end)
 
-QBCore.Commands.Add("removelawyer", "Remove someone as lawyer", {{name="id", help="id player"}}, true, function(source, args)
+QBCore.Commands.Add('removelawyer', 'Remove someone as lawyer', {{name = 'id', help = 'ID'}}, true, function(source, args)
     local Player = QBCore.Functions.GetPlayer(source)
     local playerId = tonumber(args[1])
     local OtherPlayer = QBCore.Functions.GetPlayer(playerId)
-    if Player.PlayerData.job.name == "judge" then
+    if Player.PlayerData.job.name == 'lawyer' then
         if OtherPlayer ~= nil then 
-            --OtherPlayer.Functions.SetJob("unemployed")
-            TriggerClientEvent("QBCore:Notify", OtherPlayer.PlayerData.source, "You are now unemployed.")
-            TriggerClientEvent("QBCore:Notify", source, "You were ".. OtherPlayer.PlayerData.charinfo.firstname .. " " .. OtherPlayer.PlayerData.charinfo.lastname .. " removed as a lawyer")
+            TriggerClientEvent('QBCore:Notify', OtherPlayer.PlayerData.source, 'You are now unemployed.')
+		OtherPlayer.Functions.SetJob('unemployed')
+            TriggerClientEvent('QBCore:Notify', source, 'You were '.. OtherPlayer.PlayerData.charinfo.firstname .. ' ' .. OtherPlayer.PlayerData.charinfo.lastname .. ' removed as a lawyer')
         else
-            TriggerClientEvent("QBCore:Notify", source, "This person is not present..", "error")
+            TriggerClientEvent('QBCore:Notify', source, 'This person is not present..', 'error')
         end
     else
-        TriggerClientEvent("QBCore:Notify", source, "You have no rights..", "error")
+        TriggerClientEvent('QBCore:Notify', source, 'You have no rights..', 'error')
     end
 end)
 
-QBCore.Functions.CreateUseableItem("lawyerpass", function(source, item)
+QBCore.Functions.CreateUseableItem('lawyerpass', function(source, item)
     local Player = QBCore.Functions.GetPlayer(source)
 	if Player.Functions.GetItemBySlot(item.slot) ~= nil then
-        TriggerClientEvent("qb-justice:client:showLawyerLicense", -1, source, item.info)
+        TriggerClientEvent('qb-justice:client:showLawyerLicense', -1, source, item.info)
     end
 end)
