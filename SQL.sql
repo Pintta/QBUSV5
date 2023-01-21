3939034f5e0a28efee3a2997fc68ac9e8ee58476
Pintta <69728770+Pintta@users.noreply.github.com>
2023-01-22 00:53:15 +0200

    Update SQL.sql

--- SQL.sql
+++ SQL.sql
@@ -2,6 +2,14 @@
 CREATE DATABASE `QBUS5`;
 USE QBUS5;
 
+CREATE TABLE IF NOT EXISTS `moneysafes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `safe` varchar(50) NOT NULL DEFAULT '0',
+  `money` int(11) NOT NULL DEFAULT 0,
+  `transactions` text NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+);
+
 CREATE TABLE IF NOT EXISTS `apartments` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) DEFAULT NULL,
